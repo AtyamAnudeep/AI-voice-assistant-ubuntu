dbe49cb9198445b7d63d2cb88f3fe456a6f57ffd
Anudeep Atyam <30544866+AtyamAnudeep@users.noreply.github.com>
2018-10-19 14:17:08 +0530

    Add files via upload

---
+++ AI_Assistant.py
@@ -0,0 +1,41 @@
+from gtts import gTTS 
+import speech_recognition as sr 
+import os
+import webbrowser
+import smtplib
+
+def talkToDeepu(audio):
+	print(audio)
+	tts = gTTS(text=audio,lang='en')
+	tts.save('audio.mp3')
+	os.system('mpg123 audio.mp3')
+
+def DeepsCommand():
+	r = sr.Recognizer()
+	with sr.Microphone() as source:
+		print('what is ur next command')
+		r.pause_threshold=1
+		r.adjust_for_ambient_noise(source,duration=1)
+		audio = r.listen(source)
+	try :	
+		command = r.recognize_google(audio)
+		print('you said: '+command+'\n')
+
+	except sr.UnknownVakueError:
+		assistant(DeepsCommand())
+
+	return command
+
+def assistant(command):
+	if 'open Facebook' in command:
+		chrome_path = '/usr/bin/google-chrome'
+		url = 'https://www.facebook.com'
+		webbrowser.get(chrome_path).open(url)
+
+	if 'hello' in command:
+		talkToDeepu('hello anudeep')
+
+talkToDeepu('what is ur next command')
+
+while True:
+	assistant(DeepsCommand())	 	 				
